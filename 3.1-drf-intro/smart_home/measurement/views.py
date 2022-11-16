67b6ac555977d8eff0efe02ed3b32d00ef0adc4c
Liaksej <tarusov.liaksej@gmail.com>
2022-11-16 23:05:31 +0100

    feat: added 3 view-classes for Sensor model and Measurement model

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,19 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementsSerializer
+
+
+class SensorsListView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementsListView(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementsSerializer
